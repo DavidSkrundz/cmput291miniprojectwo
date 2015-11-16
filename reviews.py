681c40ac7e93bdd9675ce8b3df12acadaaf27d70
dktrotti <dktrotti@ualberta.ca>
2015-11-16 16:06:31 -0700

    Implemented reviews.py

---
+++ reviews.py
@@ -0,0 +1,44 @@
+import sys
+
+def reviewsgen(contents):
+	outfile = open("reviews.txt", "w")
+	
+	reviews = contents.split("\n\n")
+	if reviews[-1] == "":
+		reviews = reviews[:-1]
+		
+	reviewno = 1;
+	for review in reviews:
+		outfile.write(str(reviewno) + ",")
+		
+		lines = review.split("\n")
+		for line in lines:
+			vals = line.split(": ", 1)
+			quotes = "title" in vals[0] \
+				or "profileName" in vals[0] \
+				or "summary" in vals[0] \
+				or "text" in vals[0]
+			
+			value = vals[1]
+			value = value.replace("\"", "&quot;")
+			value = value.replace("\\","\\\\")
+			if quotes:
+				print("writing quotes")
+				outfile.write("\"")
+				
+			outfile.write(value)
+			
+			if quotes:
+				outfile.write("\"")
+				
+			if "text" not in vals[0]:
+				outfile.write(",")
+		
+		outfile.write("\n")
+		reviewno += 1
+		
+	outfile.close()
+	
+if __name__ == "__main__":
+	contents = sys.stdin.read()
+	reviewsgen(contents)
