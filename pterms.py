721c29ac8deb5d00f04531c57dc01c4e476278ff
David Skrundz <david.skrundz@gmail.com>
2015-11-16 16:22:21 -0700

    took out 6 lines

--- pterms.py
+++ pterms.py
@@ -1,22 +1,19 @@
 import re
 def pterms_gen(contents):
 	lines = contents.split("\n")
-	words = []
 	reviewIndex = 0
+	out = open("pterms.txt", "w")
 	for line in lines:
 		line = line.lower()
 		if re.match("product/title: .*", line):
 			line = line[15:]
-			tempWords = []
+			words = []
 			reviewIndex += 1
 			for match in re.finditer("[0-9a-z_]{3,}", line):
 				word = match.group(0)
-				if not word in tempWords:
-					tempWords.append(word + "," + str(reviewIndex))
-			words += tempWords
-	out = open("pterms.txt", "w")
-	for word in words:
-		out.write(word + "\n")
+				if not word in words:
+					words.append(word + "," + str(reviewIndex))
+					out.write(word + "," + str(reviewIndex) + "\n")
 
 if __name__ == "__main__":
 	import sys
--- scores.py
+++ scores.py
@@ -1,18 +1,15 @@
 import re
 def scores_gen(contents):
 	lines = contents.split("\n")
-	scores = []
+	reviewNumber = 0
+	out = open("scores.txt", "w")
 	for line in lines:
 		line = line.lower()
 		if re.match("review/score: .*", line):
+			reviewNumber += 1
 			line = line[14:]
 			for match in re.finditer("\d+\.\d+", line):
-				score = match.group(0)
-				if not score in scores:
-					scores.append(score + "," + str(len(scores) + 1))
-	out = open("scores.txt", "w")
-	for score in scores:
-		out.write(score + "\n")
+				out.write(match.group(0) + "," + str(reviewNumber) + "\n")
 
 if __name__ == "__main__":
 	import sys
